744d68a662de3c934dd37653f2c6a106a24c7b32
franck-audiger <franck.audiger@gmail.com>
2025-06-24 05:50:33 +0200

    Display guard icon with count

--- plugins/ATB_Hunter/modules/ATB_Windows.js
+++ plugins/ATB_Hunter/modules/ATB_Windows.js
@@ -2,6 +2,26 @@ Window_Base.prototype.fittingHeight = function(numLines) {
     return numLines * (this.lineHeight()+2) + this.standardPadding() * 2;
 };
 
+var _ATB_drawBasicArea = Window_BattleStatus.prototype.drawBasicArea;
+Window_BattleStatus.prototype.drawBasicArea = function(rect, actor) {
+    _ATB_drawBasicArea.call(this, rect, actor);
+    this.drawActorGuardCount(actor, rect.x, rect.y);
+};
+
+Window_BattleStatus.prototype.drawActorGuardCount = function(actor, x, y) {
+    var guard = actor.guardRatio ? actor.guardRatio() : 0;
+    if (guard > 0) {
+        this.contents.fontSize -= 8;
+        var nameWidth = this.textWidth(actor.name());
+        var baseX = x + nameWidth + 4;
+        var baseY = y - this.lineHeight() / 4;
+        var iconIndex = 11; // Icon at grid position (0,11)
+        this.drawIcon(iconIndex, baseX, baseY);
+        this.drawText(guard, baseX + Window_Base._iconWidth, baseY, 48);
+        this.resetFontSettings();
+    }
+};
+
 
 Window_BattleStatus.prototype.drawAtbGauge = function(actor, x, y, width) {
     var atbRate = actor.atb() / 100;
