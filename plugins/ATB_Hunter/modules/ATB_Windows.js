eba34c5f24ab1851a3466ef44a12b88ced0af11f
franck.audiger <franck.audiger@gmail.com>
2025-06-24 06:51:01 +0200

    Work on status screen

--- plugins/ATB_Hunter/modules/ATB_Windows.js
+++ plugins/ATB_Hunter/modules/ATB_Windows.js
@@ -2,11 +2,41 @@ Window_Base.prototype.fittingHeight = function(numLines) {
     return numLines * (this.lineHeight()+2) + this.standardPadding() * 2;
 };
 
+const lineIncreaseForIcons = 0;
+
 // Increase the height of each battle status row so icons fit correctly.
 var _BS_lineHeight = Window_Base.prototype.lineHeight;
 Window_BattleStatus.prototype.lineHeight = function() {
-    return _BS_lineHeight.call(this) + 8;
+    return _BS_lineHeight.call(this) + lineIncreaseForIcons;
 };
+
+// Increase the height of each battle status row so icons fit correctly.
+var _PC_lineHeight = Window_PartyCommand.prototype.lineHeight;
+Window_PartyCommand.prototype.lineHeight = function() {
+    return _PC_lineHeight.call(this) + lineIncreaseForIcons;
+};
+
+// Increase the height of each battle status row so icons fit correctly.
+var _AC_lineHeight = Window_ActorCommand.prototype.lineHeight;
+Window_ActorCommand.prototype.lineHeight = function() {
+    return _PC_lineHeight.call(this) + lineIncreaseForIcons;
+};
+
+
+Window_BattleStatus.prototype.initialize = function() {
+    var width = this.windowWidth();
+    var height = this.windowHeight();
+    var x = 0;
+    var y = 0;
+    Window_Selectable.prototype.initialize.call(this, x, y, width, height);
+    this.refresh();
+    this.openness = 0;
+};
+
+Window_BattleStatus.prototype.windowWidth = function() {
+    return Graphics.boxWidth;
+};
+
 
 var _ATB_drawBasicArea = Window_BattleStatus.prototype.drawBasicArea;
 Window_BattleStatus.prototype.drawBasicArea = function(rect, actor) {
@@ -19,11 +49,14 @@ Window_BattleStatus.prototype.drawActorGuardCount = function(actor, x, y) {
     if (guard > 0) {
         this.contents.fontSize -= 8;
         var nameWidth = this.textWidth(actor.name());
-        var baseX = x + nameWidth + 12;
-        var baseY = y - this.lineHeight() / 4;
-        var iconIndex = 11; // Icon at grid position (0,11)
+        var baseX = x + nameWidth + 16;
+        var baseY = (y - this.lineHeight() / 4)+10;
+        var iconIndex = 322; // Icon at grid position (0,11)
         this.drawIcon(iconIndex, baseX, baseY);
-        this.drawText(guard, baseX + Window_Base._iconWidth, baseY, 48);
+        const prevFontSize = this.contents.fontSize;
+        this.contents.fontSize = 16;
+        this.drawText(guard, baseX+12, baseY-2, 48);
+        this.contents.fontSize = prevFontSize;
         this.resetFontSettings();
     }
 };
